2be1edf706a207afaa442657369c503a137cf203
Ivan Robayo <robayoivan2077@gmail.com>
2022-12-09 03:34:09 -0500

    Update

--- bikeApp.py
+++ bikeApp.py
@@ -1,1 +1,4 @@
-print("I'm a test")
+print("Bienvenido a BikerApp.")
+userName = ("Para iniciar ingresa tu nombre.")
+userBike = ("Ahora ingresa el cilindraje de tú motocicleta.")
+print("Vaya, de verdad te gusta la carretera.")
