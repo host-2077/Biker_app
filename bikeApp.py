8fe5f09c0ab02be04dd3e3945e2f5e3935f92856
Ivan Robayo <robayoivan2077@gmail.com>
2022-12-12 02:41:12 -0500

    Phase 1 Prompt tests

--- bikeApp.py
+++ bikeApp.py
@@ -1,26 +1,55 @@
-# print("Bienvenido a BikerApp.")
-# userName = ("Para iniciar ingresa tu nombre.")
-# userBike = ("Ahora ingresa el nombre de tu motocicleta, puede ser el modelo o el nombre que tu le has puesto a tu vechicículo.")
-# print random catchy phrase. 
-# print("Iniciemos con los análisis.")
-cilindraje = input("CC") # Cilindraje en Centímetros Cúbicos
-horsePower = input("HP") # Horse Power
-bikePeso = input("KG") # Peso en Kg
-userPeso = input("KG") # Peso en Kg
-user2Peso = input("KG") # Peso en Kg
-masa = float(bikePeso) + float(userPeso) + float(user2Peso) # Peso total en Km
-velocidadMaxMS = (float(horsePower) / 0.3) * 2.5 * masa # Velocidad máxima en M/s
-velocidadMaxKh = velocidadMaxMS * 3.6 / 1000 # Velocidad máxima en Km/h
-print("Tu motocicleta recorrerá la ruta elegida a una velocidad máxima de ", round(velocidadMaxKh, 2))
+# Biker app. Ver 1.0 
+# Diseñada por Ivan Robayo desde Colombia, América del Sur. 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - READ.ME - - - - 
+# Este es un programa que utiliza funciones básicas en Python para interactuar con el usuario y finalmente brindar información útil acerca de las especificaciones de la motocicleta que se está analizando entre ellas, su velocidad máxima teniendo en cuenta el peso real del usuario, su consumo de combustible y el gasto que puede generar. 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - GitHub: ivrr207 - - - -
 
-#------ PHASE 2 -----------------
-recorrido = input("KM")
+
+# Introducción al programa. 
+
+print("Bienvenido a .")
+userName = ("Para iniciar ingresa tu nombre.")
+userBike = ("Ponle un nombre a tu vehículo.")
+print("Hola", userName, "soy", userBike, "y voy a acompañarte en varias travesías. Entiendo que no conoces realmente de lo que soy capaz asi que este programa y el dispositivo que estás utilizando nos servirán como herramienta para comunicarnos efectivamente.") 
+print("Dicho esto, iniciemos con los análisis. Ya que eres todo un maniático de la carretera vamos a empezar por la velocidad máxima que puedes alcanzar con tu actual peso, es decir, en útlima instancia soy yo", userBike, "quien está haciendo todo el trabajo, tu preocupate por hacer ejercicio a ver si podemos ir un poco más rápido.")
+print("Para que todo salga bien asegurate de tener esta información: Cilindraje (CC), caballos de fuerza (HP), mi peso (" + userBike + ") (KG), tu peso (" + userName + ") (KG) y si aplica el peso de tu acompañante (KG")
+
+# Ingreso de información a las variables requeridas. 
+
+cilindraje = input("Ingresa el cilindraje (CC)")
+horsePower = input("Ingresa los caballos de fuerza (HP)")
+bikePeso = input("Ingresa el peso de la motocicleta (KG)")
+userPeso = input("¿Cuánto pesas? (KG)")
+user2Peso = input("¿Cuánto pesa tu acompañante? Si te gusta conducir la carretara solo como todo un vaquero renegado del oeste escribe 0")
+masa = float(bikePeso) + float(userPeso) + float(user2Peso)
+
+# Cálculo velocidad máxima de la motocicleta. 
+# 1. Velocidad máxima en Metros / Segundos (M/S):
+
+velocidadMaxMS = (float(horsePower) / 0.3) * 2.5 * masa 
+
+# 2. Velocidad máxima en KM/H
+
+velocidadMaxKh = velocidadMaxMS * 3.6 / 1000
+
+# 3. Resultado. 
+
+print(userName, "con tu peso actual y con la capacidad de ", userBike, "podrás dominar la carretera a una velocidad máxima de ", round(velocidadMaxKh, 2), "vaya! Que la pasma no se entere!")
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -PHASE 2 - - - -
+
+
+
+
+
+
+# recorrido = input("KM")
 # convierte los datos en float aca
 
-tiempo_estimado = recorrido / velocidadMaxKh
-horas = int(tiempo_estimado)
-minutos = int((tiempo_estimado - horas) * 60)
-print("sha la la ", horas, minutos)
+# tiempo_estimado = recorrido / velocidadMaxKh
+# horas = int(tiempo_estimado)
+# minutos = int((tiempo_estimado - horas) * 60)
+# print("sha la la ", horas, minutos)
 # la distancia es importante solo si quiero saber el tiempo en el que voy a recorrer tal distancia. Es decir, una pregunta que sirve para utioiar tales es: Cuanto tiempo gastare en un recorrido de 150 km. con chala chala la 
 # costeCombustible = # cuanto vale la gasolina 
 # velocidadEstimada = #conductorbased
