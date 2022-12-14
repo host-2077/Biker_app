e88e15948f0b088dc1117b1fa0ebacaba29a6b14
Ivan Robayo <robayoivan2077@gmail.com>
2022-12-14 02:15:51 -0500

    Working element. Done.

--- bikeApp.py
+++ bikeApp.py
@@ -2,40 +2,48 @@
 # Diseñada por Ivan Robayo desde Colombia, América del Sur. 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - READ.ME - - - - 
 # Este es un programa que utiliza funciones básicas en Python para interactuar con el usuario y finalmente brindar información útil acerca de las especificaciones de la motocicleta que se está analizando entre ellas, su velocidad máxima teniendo en cuenta el peso real del usuario, su consumo de combustible y el gasto que puede generar. 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - GitHub: ivrr207 - - - -
-
-
-# Introducción al programa. 
-
-print("Bienvenido a .")
-userName = ("Para iniciar ingresa tu nombre.")
-userBike = ("Ponle un nombre a tu vehículo.")
-print("Hola", userName, "soy", userBike, "y voy a acompañarte en varias travesías. Entiendo que no conoces realmente de lo que soy capaz asi que este programa y el dispositivo que estás utilizando nos servirán como herramienta para comunicarnos efectivamente.") 
-print("Dicho esto, iniciemos con los análisis. Ya que eres todo un maniático de la carretera vamos a empezar por la velocidad máxima que puedes alcanzar con tu actual peso, es decir, en útlima instancia soy yo", userBike, "quien está haciendo todo el trabajo, tu preocupate por hacer ejercicio a ver si podemos ir un poco más rápido.")
-print("Para que todo salga bien asegurate de tener esta información: Cilindraje (CC), caballos de fuerza (HP), mi peso (" + userBike + ") (KG), tu peso (" + userName + ") (KG) y si aplica el peso de tu acompañante (KG")
-
-# Ingreso de información a las variables requeridas. 
-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - GitHub: ivrr207 - - - - 
+import time
+# Introducción al programa.
+print("Bienvenido a IN PROCESS.")
+userName = input("Para iniciar ingresa tu nombre.")
+userBike = input("Pónle un nombre a tu vehículo.")
+message1 = "Hola ", userName, " soy ", userBike, " y voy a acompañarte en varias travesías. \nEntiendo que no conoces realmente de lo que soy capaz asi que este programa y tu teclado servirán como herramientas para comunicarnos efectivamente.\n> > > > > >\n"
+for i in message1:
+    print(i, end="", flush=True)
+    time.sleep(0.1)
+message2 = "Dicho esto, iniciemos con los análisis. \nYa que eres todo un maniático de la carretera vamos a empezar por la velocidad máxima que puedes alcanzar con tu peso actual.\n"
+for i in message2:
+    print(i, end="", flush=True)
+    time.sleep(0.1)
+message3 = "Necesitarás: \n- Cilindraje (CC) \n- Caballos de fuerza (HP) \n- Mi peso (" + userBike + ") (KG), \n- Tu peso (" + userName + ") (KG) \n- El peso de tu acompañante si es que existe. (KG)\n> > > > > >"
+for i in message3:
+    print(i, end="", flush=True)
+    time.sleep(0.1)
+# Ingreso de información a las variables requeridas.
 cilindraje = input("Ingresa el cilindraje (CC)")
 horsePower = input("Ingresa los caballos de fuerza (HP)")
 bikePeso = input("Ingresa el peso de la motocicleta (KG)")
 userPeso = input("¿Cuánto pesas? (KG)")
-user2Peso = input("¿Cuánto pesa tu acompañante? Si te gusta conducir la carretara solo como todo un vaquero renegado del oeste escribe 0")
+user2Peso = input("¿Cuánto pesa tu acompañante? (KG) > > > Si no tienes acompañante escribe '0'")
 masa = float(bikePeso) + float(userPeso) + float(user2Peso)
-
+# Notificación de análisis.
+loading = "A N A L I S A N D O D A T O S / "
+loadingprompt = loading * 3
+for i in loadingprompt:
+    print(i, end="", flush=True)
+    time.sleep(0.1)
 # Cálculo velocidad máxima de la motocicleta. 
-# 1. Velocidad máxima en Metros / Segundos (M/S):
-
-velocidadMaxMS = (float(horsePower) / 0.3) * 2.5 * masa 
-
-# 2. Velocidad máxima en KM/H
-
+# Velocidad máxima en Metros / Segundos (M/S):
+velocidadMaxMS = (float(horsePower) / 0.3) * 2.5 * masa
+# Velocidad máxima en KM/H
 velocidadMaxKh = velocidadMaxMS * 3.6 / 1000
-
-# 3. Resultado. 
-
-print(userName, "con tu peso actual y con la capacidad de ", userBike, "podrás dominar la carretera a una velocidad máxima de ", round(velocidadMaxKh, 2), "vaya! Que la pasma no se entere!")
-
+# 3. Resultado.
+print("\n")
+resultado = userName, " con tu peso actual y con la capacidad de ", userBike, " podrás dominar la carretera a una velocidad máxima de ", round(velocidadMaxKh, 2), " KM/H", "\n¡vaya! ¡Que la pasma no se entere!"
+for i in resultado:
+    print(i, end="", flush=True)
+    time.sleep(0.1)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -PHASE 2 - - - -
 
 
